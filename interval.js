39c155b82bf4ed491663520f31a5912253f4b651
rosh <rosh@zeoalliance.com>
2017-06-07 10:11:18 +0300

    fixed interval function

--- interval.js
+++ interval.js
@@ -4,13 +4,14 @@ function interval(func, wait, times) {
     var interv = function (w, t) {
         return function () {
             if (typeof t === "undefined" || t-- > 0) {
-                setTimeout(interv, w);
                 try {
                     func.call(null);
                 }
                 catch (e) {
                     t = 0;
                     throw e.toString();
+                }finally {
+                    setTimeout(interv, w);
                 }
             }
         };
